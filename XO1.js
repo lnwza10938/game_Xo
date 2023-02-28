6891b3cd197f1fbd6e1d3d15d7281356a231f7de
lnwza10938 <105684722+lnwza10938@users.noreply.github.com>
2023-02-28 16:10:48 +0700

    first commit

---
+++ XO1.js
@@ -0,0 +1,484 @@
+
+
+
+var xo00 = [1,2,3,
+            4,5,6,
+            7,8,9]
+var oo = -1;
+var IN,IN0,IN1,IN2
+var ssr=-1,ssr1=-1,ssr2=-1,ssr3=-1,ssr4=-1,ssr5=-1,ssr6=-1,ssr7=-1,ssr8=-1;
+
+var UUU = [1,1,1,
+           1,1,1,
+           1,1,1];
+
+
+
+
+  function CK1(){document.getElementById('s'+0+"").style.background = 'rgb(79, 234, 159)' ;return UUU[0]++,xo00[0]+=10,ssr =0;}
+  function CK2(){document.getElementById('s'+1+"").style.background = 'rgb(79, 234, 159)' ;return UUU[1]++,xo00[1]+=10,ssr1=1;}
+  function CK3(){document.getElementById('s'+2+"").style.background = 'rgb(79, 234, 159)' ;return UUU[2]++,xo00[2]+=10,ssr2=2;}
+  function CK4(){document.getElementById('s'+3+"").style.background = 'rgb(79, 234, 159)' ;return UUU[3]++,xo00[3]+=10,ssr3=3;}
+  function CK5(){document.getElementById('s'+4+"").style.background = 'rgb(79, 234, 159)' ;return UUU[4]++,xo00[4]+=10,ssr4=4;}
+  function CK6(){document.getElementById('s'+5+"").style.background = 'rgb(79, 234, 159)' ;return UUU[5]++,xo00[54]+=10,ssr5=5;}
+  function CK7(){document.getElementById('s'+6+"").style.background = 'rgb(79, 234, 159)' ;return UUU[6]++,xo00[6]+=10,ssr6=6;}
+  function CK8(){document.getElementById('s'+7+"").style.background = 'rgb(79, 234, 159)' ;return UUU[7]++,xo00[7]+=10,ssr7=7;}
+  function CK9(){document.getElementById('s'+8+"").style.background = 'rgb(79, 234, 159)' ;return UUU[8]++,xo00[8]+=10,ssr8=8;}
+
+
+
+    
+
+
+function Uop(){
+  IN = Math.floor(Math.random() * 9 );
+  IN0 = UUU[0]+UUU[1]+UUU[2]
+  
+  for(var i=-1; i <=8 ; i++)
+  // for(let h=-1; h <=8 ; h++ ){
+  //   if(UUU[h]==UUU[i]){
+  //     if(xo00[h]>10){if(IN==h){test()}}
+  //   console.log(i,"",h);
+  //   }
+  // }
+
+  for(let h=-1; h <=8 ; h++ ){
+      if(xo00[h]>11){if(IN==h){test();if(IN==h){test();}}}
+    // console.log("",h);
+    console.log(xo00);
+    
+  }
+  
+  // for(let h=-1; h <=8 ; h++ ){
+  //     if(xo00[h]>10){if(IN==h){test()}} 
+  // }
+
+  // for(let h=-1; h <=2 ; h++ ){
+  //   if(xo00[h]>10){if(IN==h){test()}}
+  // }
+  // for(let h=2; h <=5 ; h++ ){
+  //   if(xo00[h]>10){if(IN==h){test()}}
+  // }
+  // for(let h=5; h <=8 ; h++ ){
+  //   if(xo00[h]>10){if(IN==h){test()}} 
+  // }
+
+  
+  
+
+  // if(xo00[1]>10){if(IN==1){test()}}
+  // if(xo00[2]>10){if(IN==2){test()}}
+  // if(xo00[3]>10){if(IN==3){test()}}
+  // if(xo00[4]>10){if(IN==4){test()}}
+  // if(xo00[5]>10){if(IN==5){test()}}
+  // if(xo00[6]>10){if(IN==6){test()}}
+  // if(xo00[7]>10){if(IN==7){test()}}
+  // if(xo00[8]>10){if(IN==8){test()}}
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  if(1==1){
+     document.getElementById('s'+IN+"").style.background = 'rgb(198, 0, 0)';
+   if(xo00[IN]==8){console.log("uuuuuuuuuuuuuuuuuuuuuuu");}
+  }
+
+  return IN,IN0,IN1,IN2;
+  }
+
+
+  
+function test() {
+  // console.log(xo00);
+   Uop();
+  // console.log("oo = "+oo);
+  console.log("IN0 = "+IN0);
+  
+}
+function O() {
+  console.log(xo00);
+
+}
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // for(let h=0; h <=8 ; h+=3 ){
+  //   if(xo00[h]>10){if(IN==h){test()}}
+  // }
+  // for(let h=3; h <=8 ; h+=3 ){
+  //   if(xo00[h]>10){if(IN==h){test()}}
+  // }
+  // for(let h=5; h <=8 ; h+=3 ){
+  //   if(xo00[h]>10){if(IN==h){test()}}
+  // }
+
+  // for(let h=0; h <=8 ; h+=2 ){
+  //   if(xo00[h]>10){if(IN==h){test()}}
+  // }
+  // for(let h=3; h <=8 ; h+=2 ){
+  //   if(xo00[h]>10){if(IN==h){test()}}
+  // }
+  // for(let h=5; h <=8 ; h+=2 ){
+  //   if(xo00[h]>10){if(IN==h){test()}}
+  // }
+
+  // for(let h=0; h <=8 ; h+=1 ){
+  //   if(xo00[h]>10){if(IN==h){test()}}
+  // }
+  // for(let h=3; h <=8 ; h+=1 ){
+  //   if(xo00[h]>10){if(IN==h){test()}}
+  // }
+  // for(let h=5; h <=8 ; h+=1 ){
+  //   if(xo00[h]>10){if(IN==h){test()}}
+  // }
+
+
+
+
+  // for(let h=1; h <=8 ; h+=3 ){
+  //   if(xo00[h]>10){if(IN==h){test()}}
+  // }
+  // for(let h=4; h <=8 ; h+=3 ){
+  //   if(xo00[h]>10){if(IN==h){test()}}
+  // }
+  // for(let h=7; h <=8 ; h+=3 ){
+  //   if(xo00[h]>10){if(IN==h){test()}}
+  // }
+
+  // for(let h=1; h <=8 ; h+=2 ){
+  //   if(xo00[h]>10){if(IN==h){test()}}
+  // }
+  // for(let h=4; h <=8 ; h+=2 ){
+  //   if(xo00[h]>10){if(IN==h){test()}}
+  // }
+  // for(let h=7; h <=8 ; h+=2 ){
+  //   if(xo00[h]>10){if(IN==h){test()}}
+  // }
+
+  // for(let h=1; h <=8 ; h+=1 ){
+  //   if(xo00[h]>10){if(IN==h){test()}}
+  // }
+  // for(let h=4; h <=8 ; h+=1 ){
+  //   if(xo00[h]>10){if(IN==h){test()}}
+  // }
+  // for(let h=7; h <=8 ; h+=1 ){
+  //   if(xo00[h]>10){if(IN==h){test()}}
+  // }
+
+
+
+
+  // for(let h=2; h <=8 ; h+=3 ){
+  //   if(xo00[h]>10){if(IN==h){test()}}
+  // }
+  // for(let h=5; h <=8 ; h+=3 ){
+  //   if(xo00[h]>10){if(IN==h){test()}}
+  // }
+  // for(let h=8; h <=8 ; h+=3 ){
+  //   if(xo00[h]>10){if(IN==h){test()}}
+  // }
+
+  // for(let h=2; h <=8 ; h+=2 ){
+  //   if(xo00[h]>10){if(IN==h){test()}}
+  // }
+  // for(let h=5; h <=8 ; h+=2 ){
+  //   if(xo00[h]>10){if(IN==h){test()}}
+  // }
+  // for(let h=8; h <=8 ; h+=2 ){
+  //   if(xo00[h]>10){if(IN==h){test()}}
+  // }
+
+  // for(let h=2; h <=8 ; h+=1 ){
+  //   if(xo00[h]>10){if(IN==h){test()}}
+  // }
+  // for(let h=5; h <=8 ; h+=1 ){
+  //   if(xo00[h]>10){if(IN==h){test()}}
+  // }
+  // for(let h=8; h <=8 ; h+=1 ){
+  //   if(xo00[h]>10){if(IN==h){test()}}
+  // }
+
+
+
+
+
+  // console.log("ssr = "+ssr+" ssr1 = "+ssr1+" ssr2 = "+ssr2+" ssr3 = "+ssr3+" ssr4 = "+ssr4+" ssr5 = "+ssr5+" ssr6 = "+ssr6+" ssr7 = "+ssr7+" ssr8 = "+ssr8);
+  // console.log("oo = "+oo);
+  // console.log("IN = "+IN);
+
+  // if(xo00[0]>10){
+  //   IN = Math.floor(Math.random() * 2 );
+
+  // }
+
+
+
+
+  // if(IN==ssr ){IN = (Math.floor(Math.random() * 9 )); CK1(); if(IN==ssr ){IN = (Math.floor(Math.random() * 9 )); CK1()};}
+  // if(IN==ssr1){IN = (Math.floor(Math.random() * 9 )); CK2(); if(IN==ssr1){IN = (Math.floor(Math.random() * 9 )); CK2()};}
+  // if(IN==ssr2){IN = (Math.floor(Math.random() * 9 )); CK3(); if(IN==ssr2){IN = (Math.floor(Math.random() * 9 )); CK3()};}
+  // if(IN==ssr3){IN = (Math.floor(Math.random() * 9 )); CK4(); if(IN==ssr3){IN = (Math.floor(Math.random() * 9 )); CK4()};}
+  // if(IN==ssr4){IN = (Math.floor(Math.random() * 9 )); CK5(); if(IN==ssr4){IN = (Math.floor(Math.random() * 9 )); CK5()};}
+  // if(IN==ssr5){IN = (Math.floor(Math.random() * 9 )); CK6(); if(IN==ssr5){IN = (Math.floor(Math.random() * 9 )); CK6()};}
+  // if(IN==ssr6){IN = (Math.floor(Math.random() * 9 )); CK7(); if(IN==ssr6){IN = (Math.floor(Math.random() * 9 )); CK7()};}
+  // if(IN==ssr7){IN = (Math.floor(Math.random() * 9 )); CK8(); if(IN==ssr7){IN = (Math.floor(Math.random() * 9 )); CK8()};}
+  // if(IN==ssr8){IN = (Math.floor(Math.random() * 9 )); CK9(); if(IN==ssr8){IN = (Math.floor(Math.random() * 9 )); CK9()};}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // if(IN==ssr ){IN = (Math.floor(Math.random() * 9 ));  if(IN==ssr ){IN = (Math.floor(Math.random() * 9 )); };if(IN==ssr ){IN = (Math.floor(Math.random() * 9 ));  if(IN==ssr ){IN = (Math.floor(Math.random() * 9 )); };}}
+  // if(IN==ssr1){IN = (Math.floor(Math.random() * 9 ));  if(IN==ssr1){IN = (Math.floor(Math.random() * 9 )); };if(IN==ssr1){IN = (Math.floor(Math.random() * 9 ));  if(IN==ssr1){IN = (Math.floor(Math.random() * 9 )); };}}
+  // if(IN==ssr2){IN = (Math.floor(Math.random() * 9 ));  if(IN==ssr2){IN = (Math.floor(Math.random() * 9 )); };if(IN==ssr2){IN = (Math.floor(Math.random() * 9 ));  if(IN==ssr2){IN = (Math.floor(Math.random() * 9 )); };}}
+  // if(IN==ssr3){IN = (Math.floor(Math.random() * 9 ));  if(IN==ssr3){IN = (Math.floor(Math.random() * 9 )); };if(IN==ssr3){IN = (Math.floor(Math.random() * 9 ));  if(IN==ssr3){IN = (Math.floor(Math.random() * 9 )); };}}
+  // if(IN==ssr4){IN = (Math.floor(Math.random() * 9 ));  if(IN==ssr4){IN = (Math.floor(Math.random() * 9 )); };if(IN==ssr4){IN = (Math.floor(Math.random() * 9 ));  if(IN==ssr4){IN = (Math.floor(Math.random() * 9 )); };}}
+  // if(IN==ssr5){IN = (Math.floor(Math.random() * 9 ));  if(IN==ssr5){IN = (Math.floor(Math.random() * 9 )); };if(IN==ssr5){IN = (Math.floor(Math.random() * 9 ));  if(IN==ssr5){IN = (Math.floor(Math.random() * 9 )); };}}
+  // if(IN==ssr6){IN = (Math.floor(Math.random() * 9 ));  if(IN==ssr6){IN = (Math.floor(Math.random() * 9 )); };if(IN==ssr6){IN = (Math.floor(Math.random() * 9 ));  if(IN==ssr6){IN = (Math.floor(Math.random() * 9 )); };}}
+  // if(IN==ssr7){IN = (Math.floor(Math.random() * 9 ));  if(IN==ssr7){IN = (Math.floor(Math.random() * 9 )); };if(IN==ssr7){IN = (Math.floor(Math.random() * 9 ));  if(IN==ssr7){IN = (Math.floor(Math.random() * 9 )); };}}
+  // if(IN==ssr8){IN = (Math.floor(Math.random() * 9 ));  if(IN==ssr8){IN = (Math.floor(Math.random() * 9 )); };if(IN==ssr8){IN = (Math.floor(Math.random() * 9 ));  if(IN==ssr8){IN = (Math.floor(Math.random() * 9 )); };}}
+
+
+
+// for(let y = 0; IN==ssr ; y++){console.log("1"); CK1();}
+// for(let y = 0; IN==ssr1; y++){console.log("2"); CK2();}
+// for(let y = 0; IN==ssr2; y++){console.log("3"); CK3();}
+// for(let y = 0; IN==ssr3; y++){console.log("4"); CK4();}
+// for(let y = 0; IN==ssr4; y++){console.log("5"); CK5();}
+// for(let y = 0; IN==ssr5; y++){console.log("6"); CK6();}
+// for(let y = 0; IN==ssr6; y++){console.log("7"); CK7();}
+// for(let y = 0; IN==ssr7; y++){console.log("8"); CK8();}
+// for(let y = 0; IN==ssr8; y++){console.log("9"); CK9();}
+
+
+  // do {
+  //   let num = Math.floor(Math.random() * 9 );
+  //   arr.push(num);
+  //   arr = arr.filter((item, index) => {
+  //     return arr.indexOf(item) === index
+  //   });
+  // } while (arr.length < 9);
+  
+  // console.log(arr);
+
+
+// for(i = -1; i < 9; i++){
+//   if(i == xo00[i]){
+//     document.getElementById('s'+i+"").style.background = 'rgb(198, 0, 0)'
+//     i = 99
+//   }
+
+// }
+
+// for(var r = 0 ; r<9999; r++){
+  //   if(IN==ssr){IN = (Math.floor(Math.random() * 8 )+1);}
+  // if(IN==ssr1){IN = (Math.floor(Math.random() * 7 )+2)}
+  // if(IN==ssr2){IN = (Math.floor(Math.random() * 6 )+3)}
+  // if(IN==ssr3){IN = (Math.floor(Math.random() * 5 )+4)}
+  // if(IN==ssr4){IN = (Math.floor(Math.random() * 4 )+5)}
+  // if(IN==ssr5){IN = (Math.floor(Math.random() * 5 )+4)}
+  // if(IN==ssr6){IN = (Math.floor(Math.random() * 6 )+3)}
+  // if(IN==ssr7){IN = (Math.floor(Math.random() * 7 )+2)}
+  // if(IN==ssr8){IN = (Math.floor(Math.random() * 8 )+1)}
+  // console.log("r = "+r);
+  
+  // }
+
+// for(let y = 0; IN==ssr; y++){IN = (Math.floor(Math.random() * 9 ));}
+  // for(let y = 0; IN==ssr1; y++){IN = (Math.floor(Math.random() * 9 ));}
+  // for(let y = 0; IN==ssr2; y++){IN = (Math.floor(Math.random() * 9 ));}
+  // for(let y = 0; IN==ssr3; y++){IN = (Math.floor(Math.random() * 9 ));}
+  // for(let y = 0; IN==ssr4; y++){IN = (Math.floor(Math.random() * 9 ));}
+  // for(let y = 0; IN==ssr5; y++){IN = (Math.floor(Math.random() * 9 ));}
+  // for(let y = 0; IN==ssr6; y++){IN = (Math.floor(Math.random() * 9 ));}
+  // for(let y = 0; IN==ssr7; y++){IN = (Math.floor(Math.random() * 9 ));}
+  // for(let y = 0; IN==ssr8; y++){IN = (Math.floor(Math.random() * 9 ));}
+  
+  
+
+// function O(){
+//     do {
+//         let num = Math.floor(Math.random() * 9 );
+//         arr.push(num);
+//         arr = arr.filter((item, index) => {
+//           return arr.indexOf(item) === index
+//         });
+//       } while (arr.length < 9);
+
+//     for(i=0 ; i<= 24 ; i++){
+//         document.getElementById("test"+i+"").innerHTML = arr[i];
+//       }
+//     if(xo00[0]==0){document.getElementById('s'+jo+"").style.background = 'rgb(198, 0, 0)'}
+
+//     // if(xo00[1]==0){document.getElementById('s'+1+"").style.background = 'rgb(198, 0, 0)'}
+//     // if(xo00[2]==0){document.getElementById('s'+2+"").style.background = 'rgb(198, 0, 0)'}
+//     // if(xo00[3]==0){document.getElementById('s'+3+"").style.background = 'rgb(198, 0, 0)'}
+//     // if(xo00[4]==0){document.getElementById('s'+4+"").style.background = 'rgb(198, 0, 0)'}
+//     // if(xo00[5]==0){document.getElementById('s'+5+"").style.background = 'rgb(198, 0, 0)'}
+//     // if(xo00[6]==0){document.getElementById('s'+6+"").style.background = 'rgb(198, 0, 0)'}
+//     // if(xo00[7]==0){document.getElementById('s'+7+"").style.background = 'rgb(198, 0, 0)'}
+//     // if(xo00[8]==0){document.getElementById('s'+8+"").style.background = 'rgb(198, 0, 0)'}
+// }
+
+// function O(){
+//     i = arr[oo]
+//     var g = arr[0];
+//     if(i == 0){oo++
+//         if(i == 0){
+//         console.log("de");
+//         i = g
+//         }
+        
+//     }
+//     document.getElementById('s'+i+"").style.background = 'rgb(198, 0, 0)';
+//     console.log(i);
+    
+// }
+
+
+
+// if (oo < 3){
+//     Ox = Math.floor(Math.random() * 9)
+//     }
+
+
+
+
+// function CK11(){document.getElementById('s'+0+"").style.background = 'rgb(198, 0, 0)'}
+// function CK22(){document.getElementById('s'+1+"").style.background = 'rgb(198, 0, 0)'}
+// function CK33(){document.getElementById('s'+2+"").style.background = 'rgb(198, 0, 0)'}
+
+// function CK44(){document.getElementById('s'+3+"").style.background = 'rgb(198, 0, 0)'}
+// function CK55(){document.getElementById('s'+4+"").style.background = 'rgb(198, 0, 0)'}
+// function CK66(){document.getElementById('s'+5+"").style.background = 'rgb(198, 0, 0)'}
+
+// function CK77(){document.getElementById('s'+6+"").style.background = 'rgb(198, 0, 0)'}
+// function CK88(){document.getElementById('s'+7+"").style.background = 'rgb(198, 0, 0)'}
+// function CK99(){document.getElementById('s'+8+"").style.background = 'rgb(198, 0, 0)'}
+
+
+
+
+
+
+// var xo1 = []
+// var xo2 = []
+// var xo3 = []
+
+
+// function random(){
+//     xo1 = XO1[0]+XO1[1]+XO1[2]
+//     xo2 = XO2[0]+XO2[1]+XO2[2]
+//     xo3 = XO3[0]+XO3[1]+XO3[2]
+
+//     if(xo1 == 4){
+//         if(XO1[0]==1){
+//             document.getElementById('s'+0+"").style.background = 'rgb(198, 0, 0)'
+//         }else if(XO1[1]==1){
+//             document.getElementById('s'+1+"").style.background = 'rgb(198, 0, 0)'
+//         }else if(XO1[2]==1){
+//             document.getElementById('s'+2+"").style.background = 'rgb(198, 0, 0)'
+//         }    
+//     }
+//     if(xo1 == 5){
+//         if(XO1[0]==2){
+//             document.getElementById('s'+4+"").style.background = 'rgb(198, 0, 0)'
+//         }else if(XO1[1]==2){
+//             document.getElementById('s'+5+"").style.background = 'rgb(198, 0, 0)'
+//         }else if(XO1[2]==2){
+//             document.getElementById('s'+3+"").style.background = 'rgb(198, 0, 0)'
+//         }    
+//     }
+
+
+
+//     if(xo2 == 4){
+//         if(XO2[0]==1){
+//             document.getElementById('s'+3+"").style.background = 'rgb(198, 0, 0)'
+//         }else if(XO2[1]==1){
+//             document.getElementById('s'+4+"").style.background = 'rgb(198, 0, 0)'
+//         }else if(XO2[2]==1){
+//             document.getElementById('s'+5+"").style.background = 'rgb(198, 0, 0)'
+//         }    
+//     }
+//     if(xo2 == 5){
+//         if(XO2[0]==2){
+//             document.getElementById('s'+8+"").style.background = 'rgb(198, 0, 0)'
+//         }else if(XO2[1]==2){
+//             document.getElementById('s'+8+"").style.background = 'rgb(198, 0, 0)'
+//         }else if(XO2[2]==2){
+//             document.getElementById('s'+8+"").style.background = 'rgb(198, 0, 0)'
+//         }    
+//     }
+
+
+
+
+//     if(xo3 == 4){
+//         if(XO3[0]==1){
+//             document.getElementById('s'+8+"").style.background = 'rgb(198, 0, 0)'
+//         }else if(XO3[1]==1){
+//             document.getElementById('s'+8+"").style.background = 'rgb(198, 0, 0)'
+//         }else if(XO3[2]==1){
+//             document.getElementById('s'+8+"").style.background = 'rgb(198, 0, 0)'
+//         }    
+//     }
+//     if(xo3 == 5){
+//         if(XO3[0]==2){
+//             document.getElementById('s'+8+"").style.background = 'rgb(198, 0, 0)'
+//         }else if(XO3[1]==2){
+//             document.getElementById('s'+8+"").style.background = 'rgb(198, 0, 0)'
+//         }else if(XO3[2]==2){
+//             document.getElementById('s'+8+"").style.background = 'rgb(198, 0, 0)'
+//         }    
+//     }
+
+//     console.log(xo1);
+//     console.log(XO1);
+//     console.log(XO2);
+//     console.log(XO3);
+
+
+    
+
+    
+// }
+
+
